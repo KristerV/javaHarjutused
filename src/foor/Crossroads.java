230aa02a53f01322c2531e3c11f510522c6c9b2f
Jorma Rebane <jorma.rebane@gmail.com>
2015-09-26 14:40:42 +0300

    Added single light implementation. Crossroads now extends singlelight impl.

--- src/foor/Crossroads.java
+++ src/foor/Crossroads.java
@@ -1,11 +1,8 @@
 package foor;
 
-import javafx.application.Application;
 import javafx.stage.Stage;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class Crossroads extends Application {
+public class Crossroads extends SingleLight {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -37,39 +34,5 @@ public class Crossroads extends Application {
         greenToRed(light);
 
         light.playAnimation();
-    }
-
-    // 7s
-    private void greenIdle(TrafficLight light){
-        light.setGreen();
-        light.addPause(7.0);
-    }
-
-    // 4s
-    private void greenToRed(TrafficLight light) {
-        for (int i = 0; i < 6; ++i) {
-            light.toggleGreen();
-            light.addPause(0.5);
-        }
-        light.toggleGreen();
-
-        light.toggleYellow();
-        light.addPause(1.0);
-        light.toggleYellow();
-    }
-
-    // 10s
-    private void redIdle(TrafficLight light){
-        light.setRed();
-        light.addPause(10.0);
-    }
-
-    // 1s
-    private void redToGreen(TrafficLight light) {
-        light.toggleYellow();
-        light.addPause(1.0);
-
-        light.toggleRed();
-        light.toggleYellow();
     }
 }
