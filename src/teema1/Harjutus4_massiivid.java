d909eafc53d5b01a55d99842ac1e5c09f436303e
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-10-10 10:55:12 +0300

    massiivide harjutus

---
+++ src/teema1/Harjutus4_massiivid.java
@@ -0,0 +1,12 @@
+package teema1;
+
+/**
+ * 1. Salvesta kasutajalt 10 numbrit ja sorteeri suuruse järgi. (int[])
+ * 2. Salvesta kasutajalt 10 sõne ja sorteeri tähestikulises järjekorras. (String[])
+ * 3. Salvesta kasutajalt x arv numbreid, lõpeta küsimine kui number on 0
+ *    ja sorteeri tulemus suuruse järgi. (ArrayList)
+ *
+ * main() meetodi saad lühendiga psvm.
+ */
+public class Harjutus4_massiivid {
+}
