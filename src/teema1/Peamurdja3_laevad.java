43414681d3c2235ab9d16a8f6ec581d12950c60d
Krister Viirsaar <kviirsaa@klass2-oj-l.intra.itcollege.ee>
2015-10-11 12:14:59 +0300

    laevade lahendus

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -16,4 +16,41 @@ import java.util.Scanner;
  *    nii vertikaalselt kui ka horisontaalselt?
  */
 public class Peamurdja3_laevad {
+    public static void main(String[] args) {
+
+        int[] laud = {randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev()};
+
+        Scanner kasutaja = new Scanner(System.in);
+
+        while (!gameover(laud)) {
+            System.out.println(Arrays.toString(laud));
+            System.out.println("Sisesta number 1-"+laud.length);
+            int sisestus = kasutaja.nextInt() - 1;
+
+            int hit = laud[sisestus];
+            if (hit == 0) {
+                System.out.println("haha, mööda!");
+            } else if (hit == 2) {
+                System.out.println("kuule kuule, siia oled juba lasknud.");
+            } else if (hit == 1) {
+                System.out.println("Yea, pihtas!");
+                laud[sisestus] = 2;
+            }
+        }
+        System.out.println("Mäng läbi!");
+    }
+
+    public static int randLaev() {
+        int tulemus = (int) (Math.random() * 2);
+        return tulemus;
+    }
+
+    public static boolean gameover(int[] laud) {
+        for (int i = 0; i < laud.length; i++) {
+            if (laud[i] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
