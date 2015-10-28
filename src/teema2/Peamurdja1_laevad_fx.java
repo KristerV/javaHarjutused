970b38f6b95ad2379e35a98f4ccb37b6b8a68ab8
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-10-28 13:02:51 +0200

    instructions

--- src/teema2/Peamurdja1_laevad_fx.java
+++ src/teema2/Peamurdja1_laevad_fx.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 
 /**
  * 0. Ehita 1x1 laevade pommitamine 9x9 maatriksile, nii nagu teema1 Peamurdja3_laevad ülesanne 2 tulemus.
+ *
+ * 1. Kasuta JavaFX, et joonistada laud ja näita mängu kulgemist.
+ *      http://i200.itcollege.ee/javafx
+ *      http://i200.itcollege.ee/javafx-layout-GridPane
+ *
+ * 2. Täienda graafikat nii, et kasutaja saaks rünnata kasutades hiire klikki.
+ *
+ *
+ * 3. Kasuta JavaFXi kasutajalt nime küsimiseks enne mängu.
+ *      http://i200.itcollege.ee/javafx-TextField
  */
 public class Peamurdja1_laevad_fx {
     public static void main(String[] args) {
