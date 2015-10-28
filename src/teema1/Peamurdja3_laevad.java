86211251c873e321ba819d3db6de5f8e32deebcc
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-10-28 12:12:18 +0200

    empty the solutions from master

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -1,8 +1,5 @@
 package teema1;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 /**
  * NB! Lahenda käesolev ülesanne konsoolis. Pole vaja JavaFXi siia segada.
  *
@@ -16,52 +13,10 @@ import java.util.Scanner;
  *    nii vertikaalselt kui ka horisontaalselt?
  */
 public class Peamurdja3_laevad {
+
     public static void main(String[] args) {
-        int[][] laud = new int[9][9];
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                laud[i][j] = r();
-            }
-        }
-
-        for (int i = 0; i < 9; i++) {
-            System.out.println(Arrays.toString(laud[i]));
-        }
-
-
-
-        Scanner kasutaja = new Scanner(System.in);
-
-        /*while (gameover(laud)) {
-            System.out.println(Arrays.toString(laud));
-            System.out.println("Siesta üks nr");
-            int sisestus = kasutaja.nextInt();
-            int hit = laud[sisestus];
-            if (hit == 0) {
-                System.out.println("hahaa, mööda.");
-            } else if (hit == 1) {
-                System.out.println("ou fakk... pihtas");
-                laud[sisestus] = 2;
-            } else if (hit == 2) {
-                System.out.println("jou, kuule....... sa juba lasid siin põhja.");
-            }
-        }*/
-        System.out.println("Jee, võitsid (oli ka raske?)");
     }
 
-    public static boolean gameover(int[] laud) {
-        for (int i = 0; i < laud.length; i++) {
-            if (laud[i] == 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static int r() {
-        return (int) (Math.random() * 2);
-    }
 }
 
 
