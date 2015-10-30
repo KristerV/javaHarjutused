a7fc8616fcca7cb1659a62f81226c0e38deac27b
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-10-30 23:16:11 +0200

    kordamine

---
+++ src/teema2/Kordamine1_laevad_maatriksil.java
@@ -0,0 +1,7 @@
+package teema2;
+
+/**
+ * Loo laevade pommitamine, kus on vaid 1x1 laevad ja kasutaja saam arvuti laevasid pommitada 9x9 maatriksil.
+ */
+public class Kordamine1_laevad_maatriksil {
+}
